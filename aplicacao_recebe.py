1b901fa4757ed8d0341dd3432800f89bafae2549
Bruno Costa <brunex887@gmail.com>
2018-08-27 11:06:49 -0300

    Projeto 3

---
+++ aplicacao_recebe.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação
+####################################################
+
+print("comecou")
+
+from enlace import *
+import time
+
+
+
+
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer a
+# comunicaçao
+# Serial Com Port
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM10"                  # Windows(variacao de)
+
+
+
+print("porta COM aberta com sucesso")
+
+
+
+def main():
+    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+    com = enlace(serialName)
+
+    # Ativa comunicacao
+    com.enable()
+
+    # Faz a recepção dos dados
+    print ("Recebendo dados .... ")
+
+        
+    rxBuffer, nRx = com.getData()
+
+    print("Tempo estimado: " + str((nRx*10*2)/com.fisica.baudrate)+ " segundos.")
+    # log
+    print ("Lido              {} bytes ".format(nRx))
+    
+    '''img = open('MEC.jpg', 'wb')
+    img.write(bytearray(rxBuffer))
+    img.close()'''
+
+    print(rxBuffer)
+
+    
+
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    com.disable()
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
